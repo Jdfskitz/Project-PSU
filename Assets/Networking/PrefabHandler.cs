00c719f0198c359f561fa56846e04b267bac874c
Jdfskitz <JustinDFitzpatrick@Gmail.com>
2017-09-19 00:45:25 -0600

    Network Spawning
    Finished the Prefab spawning base script for networking.
    
    Just need to setup SQL Server and create the database. The table creation Query is in the comments.

--- Assets/Networking/PrefabHandler.cs
+++ Assets/Networking/PrefabHandler.cs
@@ -14,29 +14,73 @@ public string SQL_HOST = "";
 private string connectionString = null;
 MySql.Data.MySqlClient.MySqlConnection cnn;
 
+
+private string goName;
+private int goID;
+private int goTransformX;
+private int goTransformY;
+private int goTransformZ;
+private int goRotateX;
+private int goRotateY;
+private int goRotateZ;
+
+
+
 public void Start()
 {
+
 connectionString = "Data Source=SQL_HOST,SQL_PORT;Initial Catalog=SQL_DATABASE_NAME;User ID=SQL_USERNAME;Password=SQL_PASSWORD";
 cnn = new MySql.Data.MySqlClient.MySqlConnection(connectionString);
-}
 
-public void Update()
-{
     try {
+        Debug.Log("Connecting to MySQL");
         cnn.Open();
-            //* On Start of Game Instantiate GameObjects */
-            //* 1 GameObject for Every Array in SQL */
-            //* Add Range Detection to Each Game Object */
+        MySql.Data.MySqlClient.MySqlBulkLoader ObjectLoader = new MySql.Data.MySqlClient.MySqlBulkLoader(cnn);
 
-            //* If SQLGRAB.ID = 1 Spawn Skeleton */
-            //* If SPAWNINFO[i].model == 1; 
-            //* GameObject go = (GameObject)Instantiate(Resources.Load("MyPrefab"),new Vector3(SQLGRAB.x,SQLGRAB.y,SQLGRAB.z)
-            //*										,Quaternion.Euler(SQLGRAB.rotationx,SQLGRAB.rotationy,SQLGRAB.rotationz)); */
+        //*CREATE TABLE GameObjects (Name VARCHAR(100) NOT NULL, GameObjectID INTEGER, TransformY INTEGER, TransformY INTEGER, TransformZ INTEGER, RotateX INTEGER, RotateY INTEGER, RotateZ INTEGER); */
+
+
+
+        string sql = "SELECT Name,GameObjectID,TransformX,TransformY,TransformZ,RotateX,RotateY,RotateZ FROM GameObjects";
+
+        MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(sql, cnn);
+
+        MySql.Data.MySqlClient.MySqlDataReader goInfo = cmd.ExecuteReader();
+
+        
+            while(goInfo.Read())
+            {
+                goName = (string)goInfo[0];
+                goID = (int)goInfo[1];
+                goTransformX = (int)goInfo[2];
+                goTransformY = (int)goInfo[3];
+                goTransformZ = (int)goInfo[4];
+                goRotateX = (int)goInfo[5];
+                goRotateY = (int)goInfo[6];
+                goRotateZ = (int)goInfo[7];
+            }
+
+            //Skeleton Spawner
+            if(goID == 1)
+            {
+            GameObject go = (GameObject)Instantiate(Resources.Load("Skeleton"),new Vector3(goTransformX,goTransformY,goTransformZ)
+            									,Quaternion.Euler(goRotateX,goRotateY,goRotateZ));
+            }
+       
+        goInfo.Close();
         cnn.Close();
     }
     catch
     {
+        Debug.Log("Failed Connection");
     }
+
+
+}
+
+public void Update()
+{
+
 }
 
 
