449f363cb25958e42578a063bf28fad091687b2a
Jdfskitz <JustinDFitzpatrick@Gmail.com>
2017-09-21 12:52:33 -0600

    Reorganized PrefabHandler

--- Assets/Networking/PrefabHandler.cs
+++ Assets/Networking/PrefabHandler.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using MySql.Data.MySqlClient;
-
 public class PrefabHandler : MonoBehaviour {
 
 public string SQL_DATABASE_NAME = "";
@@ -13,8 +12,6 @@ public string SQL_HOST = "";
 
 private string connectionString = null;
 MySql.Data.MySqlClient.MySqlConnection cnn;
-
-public Vector3 thisPosition;
 private string goName;
 private int goID;
 public int goTransformX;
@@ -28,160 +25,98 @@ public int updateWaitTimer;
 private int goIndex;
 
 private int isOpen;
-private int rowNumber;
-
-private int [] countid;
-private string GameObjectsTable = "GameObjects";
-private int key;
 int i = 0;
 bool t;
 int count = 0;
 private GameObject go;
 
-
-
 public void Start ()
 {
-    SQLInitializer();
-
-
-    try {
-        Debug.Log("Connecting to MySQL");
-        cnn.Open();
-        Debug.Log("Success");
-
-        MySql.Data.MySqlClient.MySqlBulkLoader ObjectLoader = new MySql.Data.MySqlClient.MySqlBulkLoader(cnn);
-
-        //*CREATE TABLE GameObjects (Name VARCHAR(100) NOT NULL, GameObjectID INTEGER, TransformX INTEGER, TransformY INTEGER, TransformZ INTEGER, RotateX INTEGER, RotateY INTEGER, RotateZ INTEGER); */
-        //*INSERT INTO GameObjects (Name, GameObjectID, TransformX, TransformY, TransformZ, RotateX, RotateY, RotateZ) VALUES ( "" , , , , , , , ); */
-        //*EX: INSERT INTO GameObjects (Name, GameObjectID, TransformX, TransformY, TransformZ, RotateX, RotateY, RotateZ) VALUES ( "Skeleton" , 1, 295, 10, 120, 0, 80, 0); */
-
-        string sql = "SELECT Name,GameObjectID,TransformX,TransformY,TransformZ,RotateX,RotateY,RotateZ,id,isOpen FROM GameObjects";
-         
-        MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(sql, cnn);
-
-        MySql.Data.MySqlClient.MySqlDataReader goInfo = cmd.ExecuteReader();
-        
-            while(goInfo.Read())
-            {
-                goName = (string)goInfo[0];
-                goID = (int)goInfo[1];
-                goTransformX = (int)goInfo[2];
-                goTransformY = (int)goInfo[3];
-                goTransformZ = (int)goInfo[4];
-                goRotateX = (int)goInfo[5];
-                goRotateY = (int)goInfo[6];
-                goRotateZ = (int)goInfo[7];
-                goIndex = (int)goInfo[8];
-                isOpen = (int)goInfo[9];
-//                count = (int)countCMD.ExecuteScalar();
- 
-                    if(goID == 1 && isOpen == 1)
-                    {
-                    go = (GameObject)Instantiate(Resources.Load("Skeleton"),new Vector3(goTransformX,goTransformY,goTransformZ)
-                                                        ,Quaternion.Euler(goRotateX,goRotateY,goRotateZ));
-
-                    go.transform.position = new Vector3(goTransformX,goTransformY,goTransformZ);
-                    
-                    go.GetComponent<CombatHandler>().pID = goIndex;
-                    isOpen = 0;
-
-                    }
-
-        } 
-
-         
-
-        //Skeleton Spawner
-        goInfo.Close();
-
-
-    }
-    catch(MySql.Data.MySqlClient.MySqlException sqlEx)
-    {
-        Debug.Log("Failed Connection");
-        SQLTermination();
-        
-    }
-
-   
-}
-
-
-public void Update()
-{       
-        
-        try{
-        string sql = "SELECT Name,GameObjectID,TransformX,TransformY,TransformZ,RotateX,RotateY,RotateZ,id,isOpen FROM GameObjects";
-
-        MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(sql, cnn);
-
-        MySql.Data.MySqlClient.MySqlDataReader goInfo = cmd.ExecuteReader();
-
-        while(goInfo.Read())
-        {
-                    goName = (string)goInfo[0];
-                    goID = (int)goInfo[1];
-                    goTransformX = (int)goInfo[2];
-                    goTransformY = (int)goInfo[3];
-                    goTransformZ = (int)goInfo[4];
-                    goRotateX = (int)goInfo[5];
-                    goRotateY = (int)goInfo[6];
-                    goRotateZ = (int)goInfo[7];
-                    goIndex = (int)goInfo[8];
-                    isOpen = (int)goInfo[9];
-        }
-        
-        //MySql.Data.MySqlClient.MySqlCommand Update = new MySql.Data.MySqlClient.MySqlCommand("UPDATE `Prefabs`.`GameObjects` SET `TransformX`= '" + go.GetComponent<CombatHandler>().TransformX + "' `TransformY`= '" + go.GetComponent<CombatHandler>().TransformY + "' `TransformZ`= '" + go.GetComponent<CombatHandler>().TransformZ + "';" ,cnn);
-        cmd.CommandText = "UPDATE `Prefabs`.`GameObjects` SET `TransformX`= '" + go.GetComponent<CombatHandler>().TransformX + "' `TransformY`= '" + go.GetComponent<CombatHandler>().TransformY + "' `TransformZ`= '" + go.GetComponent<CombatHandler>().TransformZ + "';" ;
-        
-        cmd.ExecuteReader();
-
-        Debug.Log("Prefab position update");
-        }catch(MySql.Data.MySqlClient.MySqlException sqlEx){
-
-        }
-
-        /*if(!t)
-        {
-            StartCoroutine(UpdatePosition());
-            t = true;
-        }*/
-
-
-
-
-}
-
-
-
-	/*public IEnumerator UpdatePosition()
-	{
-        SQLInitializer();
-
-        yield return new WaitForSeconds(updateWaitTimer);
-        MySql.Data.MySqlClient.MySqlCommand Update = new MySql.Data.MySqlClient.MySqlCommand("UPDATE `Prefabs`.`GameObjects` SET `TransformX`= '" + go.GetComponent<CombatHandler>().TransformX + "' `TransformY`= '" + go.GetComponent<CombatHandler>().TransformY + "' `TransformZ`= '" + go.GetComponent<CombatHandler>().TransformZ + "';" ,cnn);
-        Debug.Log("Prefab Position Update");
-        t = false;
-        
-
-    }*/
-
-
-    public void SQLInitializer()
-    {
         connectionString = "server=" + SQL_HOST + ";" + "database=" + SQL_DATABASE_NAME + ";" + "user=" + SQL_USERNAME + ";" + "password=" + SQL_PASSWORD + ";" + "port=" + SQL_PORT + ";";
+        MySql.Data.MySqlClient.MySqlBulkLoader ObjectLoader = new MySql.Data.MySqlClient.MySqlBulkLoader(cnn);
+        using(cnn = new MySql.Data.MySqlClient.MySqlConnection(connectionString))
+        {
+     
 
-        cnn = new MySql.Data.MySqlClient.MySqlConnection(connectionString);
+        try {
+                
+            cnn.Open();
+            Debug.Log("Success");       
+
+                
+
+                //*CREATE TABLE GameObjects (Name VARCHAR(100) NOT NULL, GameObjectID INTEGER, TransformX INTEGER, TransformY INTEGER, TransformZ INTEGER, RotateX INTEGER, RotateY INTEGER, RotateZ INTEGER); */
+                //*INSERT INTO GameObjects (Name, GameObjectID, TransformX, TransformY, TransformZ, RotateX, RotateY, RotateZ) VALUES ( "" , , , , , , , ); */
+                //*EX: INSERT INTO GameObjects (Name, GameObjectID, TransformX, TransformY, TransformZ, RotateX, RotateY, RotateZ) VALUES ( "Skeleton" , 1, 295, 10, 120, 0, 80, 0); */
+
+                string GameObjectsRead = "SELECT Name,GameObjectID,TransformX,TransformY,TransformZ,RotateX,RotateY,RotateZ,id,isOpen FROM GameObjects";
+                MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(GameObjectsRead, cnn);
+                MySql.Data.MySqlClient.MySqlDataReader GameObjectsDB = cmd.ExecuteReader();
+ 
+                    while(GameObjectsDB.Read())
+                    {
+                        goName = (string)GameObjectsDB[0];
+                        goID = (int)GameObjectsDB[1];
+                        goTransformX = (int)GameObjectsDB[2];
+                        goTransformY = (int)GameObjectsDB[3];
+                        goTransformZ = (int)GameObjectsDB[4];
+                        goRotateX = (int)GameObjectsDB[5];
+                        goRotateY = (int)GameObjectsDB[6];
+                        goRotateZ = (int)GameObjectsDB[7];
+                        goIndex = (int)GameObjectsDB[8];
+                        isOpen = (int)GameObjectsDB[9];                  
+                            Debug.Log("goInfo Read");
+                            
+                            if(goID == 1 && isOpen == 1)
+                            {
+                            go = (GameObject)Instantiate(Resources.Load("Skeleton"),new Vector3(goTransformX,goTransformY,goTransformZ),Quaternion.Euler(goRotateX,goRotateY,goRotateZ));
+                            go.transform.position = new Vector3(goTransformX,goTransformY,goTransformZ);
+                            go.GetComponent<CombatHandler>().pID = goIndex;
+                            isOpen = 0;
+                            Debug.Log("Object Spawned");
+                            //Skeleton Spawner
+                            }
+                }
+                GameObjectsDB.Close();     
+                
+            }catch(MySql.Data.MySqlClient.MySqlException sqlEx){
+                    Debug.Log("Failed Connection");  
+                    cnn.Close();
+            }
+        }
     }
 
-
-    public void SQLTermination()
+/* 
+    void OnDependencyChange(object sender, SqlNotificationEventArgs e )
     {
-        cnn.Close();
+    // Handle the event (for example, invalidate this cache entry).
     }
 
+
+        void UpdatePosition()
+    {
+        //MySql.Data.MySqlClient.MySqlCommand Update = new MySql.Data.MySqlClient.MySqlCommand("UPDATE `Prefabs`.`GameObjects` SET `TransformX`= '" + go.GetComponent<CombatHandler>().TransformX + "' `TransformY`= '" + go.GetComponent<CombatHandler>().TransformY + "' `TransformZ`= '" + go.GetComponent<CombatHandler>().TransformZ + "';" ,cnn);
+        //Update.BeginExecuteNonQuery();
+        string GameObjectsRead = "SELECT Name,GameObjectID,TransformX,TransformY,TransformZ,RotateX,RotateY,RotateZ,id,isOpen FROM GameObjects";
+        MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(GameObjectsRead, cnn);
+        SqlDependency dependency = new SqlDependency(cmd);
+        //cmd.CommandText = "UPDATE `Prefabs`.`GameObjects` SET `TransformX`= '" + go.GetComponent<CombatHandler>().TransformX + "' `TransformY`= '" + go.GetComponent<CombatHandler>().TransformY + "' `TransformZ`= '" + go.GetComponent<CombatHandler>().TransformZ + "';" ;        
+        goTransformX = go.GetComponent<CombatHandler>().TransformX;
+        goTransformY = go.GetComponent<CombatHandler>().TransformY;
+        goTransformZ = go.GetComponent<CombatHandler>().TransformZ;
+        // Maintain the refence in a class member.
+        // Subscribe to the SqlDependency event.
+        cmd.OnChange += new MySql.Data.MySqlClient.OnChangeEventHandler(OnDependencyChange);
+        // Execute the command.
+        command.ExecuteReader();
+    }
+
+*/
 }
+
+
+
+
 
 
 
