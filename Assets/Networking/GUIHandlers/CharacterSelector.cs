85509c3909db10dd72c4b40f02f7716e935d2c7d
Jdfskitz <JustinDFitzpatrick@Gmail.com>
2017-09-27 00:38:47 -0600

    Some Pseudo

--- Assets/Networking/GUIHandlers/CharacterSelector.cs
+++ Assets/Networking/GUIHandlers/CharacterSelector.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterSelector : MonoBehaviour{
 
@@ -8,8 +9,10 @@ GameObject charSelector;
 [SerializeField]
 Transform Background;
 
-
+public Text charNameText;
+public int selectedChar;
 public string [] nameList; 
+public List <int> charSelected;
 
 	// Use this for initialization
 	void Start () {
@@ -19,15 +22,30 @@ public string [] nameList;
 	for(int i = 0; i < nameList.Length; i++)
 		{
 			charSelector = (GameObject)Instantiate(Resources.Load("charSelection"),new Vector3(Screen.width/10,Screen.height/2 + i*60,0),Quaternion.Euler(0,0,0));
+			charSelector.GetComponentInChildren<Text>().text = nameList[i];
+			charSelected.Add(i);
+			charSelector.GetComponent<OnButtonSelected>().selectedNo = i;
 			charSelector.transform.SetParent(Background);
 		}
 		//Instantiate Selection Buttons
 	}
 
+	public void characterCreation()
+	{
+		//on character creation, insert into accounts where accounts == AccountID character Number
+		//set Max Character Limit
+	}
+
 	public void setSelected(){
-		//Set int Selected to character ID value in DB
+	
+		selectedChar = charSelector.GetComponent<OnButtonSelected>().selectedNo;
+
 	}
 	public void Login(){
-		//Load Scene Game
+		//do a read, find SelectedNO Where AccountID = charAccountID
+		//Select GameObjectID Where SelectedNoID
+		
+		//Load Scene
+		//Instantiate GameObject under account.
 	}
 }
